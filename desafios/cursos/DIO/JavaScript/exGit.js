8876a488b98698d47c9eda163eeeeb1c2081d364
Raoni Lucas <RaoniLucas123@gmail.com>
2025-12-08 17:09:39 -0300

    corrigindo erro no texto do enunciado

--- desafios/cursos/DIO/JavaScript/exGit.js
+++ desafios/cursos/DIO/JavaScript/exGit.js
@@ -2,7 +2,7 @@
 
 Objetivo 
 
-Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar as mensasgens.
 
 */
 
