f0974cfd9ba8fb4460d26e8d5fdc54686ce64fd4
Raoni Lucas <RaoniLucas123@gmail.com>
2025-12-09 16:03:16 -0300

    corrigindo  atividade

--- desafios/cursos/DIO/JavaScript/desafio02.js
+++ desafios/cursos/DIO/JavaScript/desafio02.js
@@ -1,35 +1,40 @@
-function rankBalance() {
+function rankBalance(numberOfWins) {
    let users = [
-       ["Aragorn", 15],
-       ["Gandalf", 75],
-       ["Legolas", 30],
-       ["Gimli", 8],
-       ["Frodo", 102],
-       ["Samwise", 45],
-       ["Galadriel", 90]
+       ["Aragorn"],
+       ["Gandalf"],
+       ["Legolas"],
+       ["Gimli"],
+       ["Frodo"],
+       ["Samwise"],
+       ["Galadriel"]
    ];
    
-   for (let [name, numberOfWins] of users) {
+   users.forEach((user, index) => {
+      let wins = numberOfWins[index] !== undefined ? numberOfWins[index] : 0;
+      user.push(wins);
+   });
+   
+   for (let [name, wins] of users) {
       let patent;
       
-      if (numberOfWins < 10) {
+      if (wins < 10) {
          patent = 'Ferro';
-      } else if (numberOfWins >= 10 && numberOfWins <= 20) {
+      } else if (wins >= 10 && wins <= 20) {
          patent = 'Bronze';
-      } else if (numberOfWins > 20 && numberOfWins <= 50) {
+      } else if (wins > 20 && wins <= 50) {
          patent = 'Prata';
-      } else if (numberOfWins > 50 && numberOfWins <= 80) {
+      } else if (wins > 50 && wins <= 80) {
          patent = 'Ouro';
-      } else if (numberOfWins > 80 && numberOfWins <= 90) {
+      } else if (wins > 80 && wins <= 90) {
          patent = 'Diamante';
-      } else if (numberOfWins > 90 && numberOfWins <= 100) {
+      } else if (wins > 90 && wins <= 100) {
          patent = 'Lendário';
-      } else if (numberOfWins >= 101) {
+      } else if (wins >= 101) {
          patent = 'Imortal';
       }
       
-      console.log(`O herói ${name} tem um saldo de vitória de ${numberOfWins} e está no nível ${patent}`)
+      console.log(`O herói ${name} tem um saldo de vitória de ${wins} e está no nível ${patent}`);
    }
 }
 
-rankBalance();
+rankBalance([5, 25, 35, 55, 85, 95, 105]);
